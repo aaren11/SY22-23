b007c0e156b946c3d75da3172f0ca3475be48493
atdic365 <atdic365@learner.dvusd.org>
2022-08-22 09:11:24 -0700

    Monday Update

--- C#/mupt answers/mupt answers/Program.cs
+++ C#/mupt answers/mupt answers/Program.cs
@@ -3,40 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace mupt_answers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string answer1;
-            string answer2;
-            string answer3;
-            int correct=0;
-            Console.WriteLine("How Many moons does earth have");
-            Console.WriteLine("a,1");
-            Console.WriteLine("b,2");
-            Console.WriteLine("c,3");
-            answer1 =(Console.ReadLine());
-            Console.WriteLine("what is 15+2");
-            Console.WriteLine("a,17");
-            Console.WriteLine("b,24");
-            Console.WriteLine("c,353");
-            answer2 =(Console.ReadLine());
-            Console.WriteLine("what is the color ");
-            Console.WriteLine("A,15");
-            Console.WriteLine("b,Red");
-            Console.WriteLine("c,dog");
-            answer3 =(Console.ReadLine());
-            if (answer1 == "a") { correct++; }
-            if (answer2 == "a") { correct++; }
-            if (answer3 == "b") { correct++; }
-            Console.WriteLine(correct);
-            Console.ReadLine();
-            Console.WriteLine("Your Score is {0},", correct);
+            showQuestion("How Many moons does earth have", " a)1", " b)2", " c)0,",
+" d)6", "a");
+
+            showQuestion("What is 15+2", " a)1", " b)2", " c)17,", " d)6", "c");
+
+            showQuestion("What is a color ", " a)15", " b)red", " c)dog,", " d)6",
+"c");
+
+            showQuestion("What is 1+1", " a)1", " b)2", " c)0,", " d)6", "b");
             Console.ReadKey();
-           
+        }
+        public static void showQuestion(string question,
+                                        string answer1,
+                                        string answer2,
+                                        string answer3,
+                                        string answer4,
+                                        string correct)
+        {
+            Console.WriteLine(question);
+            Console.WriteLine(answer1);
+            Console.WriteLine(answer2);
+            Console.WriteLine(answer3);
+            Console.WriteLine(answer4);
+            string answer = Console.ReadLine();
+            if (answer.Equals(correct))
+            {
+                Console.WriteLine("you got it right ");
+                Console.ReadKey();
+            }
 
         }
-    } }
+    }
+}
